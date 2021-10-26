27be4907a052d9023a890baca71ee5d1bf81fcd8
murphbra <murphbra@oregonstate.edu>
2021-10-25 23:50:30 -0600

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const app = express();
 
-const Datastore = require('@google-cloud/datastore');
+const {Datastore} = require('@google-cloud/datastore');
 const bodyParser = require('body-parser');
 
 const datastore = new Datastore();
