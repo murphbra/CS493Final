268b5ecab5cee6fe9aead84462c04ceab3a498d6
murphbra <murphbra@oregonstate.edu>
2021-12-06 12:04:02 -0800

    update

--- server.js
+++ server.js
@@ -125,36 +125,43 @@ function get_loads_count(){
 	const q = datastore.createQuery(LOAD);
 	return datastore.runQuery(q).then( (entities) => {
 			return entities[0].map(fromDatastore);
-		}); 
+		}).then ((loads) => {
+            return loads.length; 
+        })
 }
 
 function get_loads(req){
     const results = {};
-    results.total_items_in_collection = 5; 
-    var q = datastore.createQuery(LOAD).limit(5);
-    if(Object.keys(req.query).includes("cursor")){
-        q = q.start(req.query.cursor);
-    }
-	return datastore.runQuery(q).then( (entities) => {
-            results.items = entities[0].map(fromDatastore);
+    get_loads_count().then((count) => {
+        results.total_items_in_collection = count 
+        return results; 
+    }).then((results) => {
+        //results.total_items_in_collection = 5; 
+        var q = datastore.createQuery(LOAD).limit(5);
+        if(Object.keys(req.query).includes("cursor")){
+            q = q.start(req.query.cursor);
+        }
+        return datastore.runQuery(q).then( (entities) => {
+                results.items = entities[0].map(fromDatastore);
 
-            for(i=0;i<results.items.length;i++)
-            {
-                results.items[i].self = "https://portfolioproject-334304.wm.r.appspot.com/loads/" + results.items[i].id;
-                if(results.items[i].carrier != null)
+                for(i=0;i<results.items.length;i++)
                 {
-                    results.items[i].carrier.self = "https://portfolioproject-334304.wm.r.appspot.com/boats/" + results.items[i].carrier.id; 
+                    results.items[i].self = "https://portfolioproject-334304.wm.r.appspot.com/loads/" + results.items[i].id;
+                    if(results.items[i].carrier != null)
+                    {
+                        results.items[i].carrier.self = "https://portfolioproject-334304.wm.r.appspot.com/boats/" + results.items[i].carrier.id; 
+                    }
                 }
-            }
 
-            if(entities[1].moreResults !== Datastore.NO_MORE_RESULTS ){
-                results.next = "https://portfolioproject-334304.wm.r.appspot.com/loads/" + "?cursor=" + entities[1].endCursor;
-            }
-            else {
-                results.next = "No more results"; 
-            }
-			return results;
-		});
+                if(entities[1].moreResults !== Datastore.NO_MORE_RESULTS ){
+                    results.next = "https://portfolioproject-334304.wm.r.appspot.com/loads/" + "?cursor=" + entities[1].endCursor;
+                }
+                else {
+                    results.next = "No more results"; 
+                }
+                return results;
+            });
+    })
 }
 
 function delete_load(id) {
